3c99e18375faf4871af42c68b09174cb928793f6
ryanhburbank <ryanhburbank@gmail.com>
2013-03-17 00:13:58 -0700

    Add downcase test & refactor before_save callback

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,7 +14,9 @@ class User < ActiveRecord::Base
   attr_accessible :email, :name, :password, :password_confirmation
   has_secure_password
 
-  before_save { |user| user.email = email.downcase }
+  before_save { email.downcase! } 
+  #bang method, equivalent to { |user| user.email = email.downcase}
+  # or { |user| email.downcase!}
 
   validates :name, presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
